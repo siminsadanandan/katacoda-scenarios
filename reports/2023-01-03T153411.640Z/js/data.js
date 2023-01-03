13c22eb71c12d5100342fe71e6b4fe2c1edaa10a
root <simin.sadanandan@gmail.com>
2023-01-03 15:37:43 +0000

    added

---
+++ reports/2023-01-03T153411.640Z/js/data.js
@@ -0,0 +1,37 @@
+var data = {
+	"testScripts": [
+		{
+			"name": "./my-element-project.perf.ts",
+			"iterationResults": [
+				{
+					"name": "Iteration 1",
+					"duration": 20997,
+					"stepResults": [
+						{
+							"name": "Start",
+							"status": "passed",
+							"subTitle": "",
+							"duration": 1846
+						},
+						{
+							"name": "Step 2 find first heading",
+							"status": "passed",
+							"subTitle": "",
+							"duration": 77
+						}
+					]
+				}
+			],
+			"duration": 20997
+		}
+	],
+	"executionInfo": {
+		"time": "Jan 03, 2023, 03:33:45 PM",
+		"mode": "headless with config file",
+		"os": "Linux 5.4",
+		"elementVersion": "2.0.4",
+		"nodeVersion": "v14.21.1",
+		"duration": 20997
+	},
+	"scriptsWithError": []
+}
